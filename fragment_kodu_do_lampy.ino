8ca97b63400df60f3358d479953ce96d6acd8281
tblisty <53945069+tblisty@users.noreply.github.com>
2021-11-09 22:22:21 +0100

    Deklaracja zmiennych

--- fragment_kodu_do_lampy.ino
+++ fragment_kodu_do_lampy.ino
@@ -1,11 +1,16 @@
 //Fragment kodu
 //ja nie wiem, czy coś z tego wyjdzie
 
+uint8_t rainbowRepetitionCounter = 4; // na przykład
+uint8_t activBottomRainbowLeds = 100; // na przykład
+uint8_t licznik_petli_koloru_teczy_dolnej = 0;
+uint8_t w = 0;
+
 void loop() {
 	for (uint8_t j=0; j < rainbowRepetitionCounter ; j++ ) {
-        for (uint8_t i=(j*activBottomRainbowLeds/rainbowRepetitionCounter); i <= ((j+1)*activBottomRainbowLeds/rainbowRepetitionCounter) ; i++) {
+		for (uint8_t i=(j*activBottomRainbowLeds/rainbowRepetitionCounter); i <= ((j+1)*activBottomRainbowLeds/rainbowRepetitionCounter) ; i++) {
 			w=(( (i*256/activBottomRainbowLeds-256/rainbowRepetitionCounter) + licznik_petli_koloru_teczy_dolnej) & 255);
-        }
-    }
+			}
+		}
 	licznik_petli_koloru_teczy_dolnej++;
-}
+	}
